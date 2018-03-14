9045020f6f009a01f26fe6ee61a4b6d61a255742
logonrm <logonrm@fiap.com.br>
2018-03-14 20:46:37 -0300

    acabou segunda aula

--- 12-WS-Restful-Server/src/br/com/fiap/ads/dbe/ProdutoResource.java
+++ 12-WS-Restful-Server/src/br/com/fiap/ads/dbe/ProdutoResource.java
@@ -5,14 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import br.com.fiap.ads.dbe.to.ProdutoTO;
+
 @Path("/produto")
 public class ProdutoResource {
 
 	//@Path("/buscar")					posso colocar mas não preciso
 	@GET								/*vou buscar*/
-	@Produces(MediaType.TEXT_PLAIN)
-	public String olaMundo() {
-		return "Ola WS Restful";
+	@Produces(MediaType.APPLICATION_JSON)
+	public ProdutoTO criar() {
+		return  new ProdutoTO(1, "Coxinha", "Coxinha de frango", 3.5, true);
 	}
 
 	
